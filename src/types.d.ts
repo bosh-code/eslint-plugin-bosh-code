773b1f4b2f20c28c81795cfd4fc81f6fa5799858
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-08-29 10:20:20 +1200

    fix types

---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+declare module 'eslint-plugin-eslint-comments';
+declare module 'eslint-plugin-jsx-a11y';
+declare module 'eslint-plugin-no-unary-plus';
+declare module 'eslint-plugin-promise';
+declare module 'eslint-plugin-react-perf';
--- types.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'eslint-plugin-eslint-comments';
-declare module 'eslint-plugin-promise';
-declare module 'eslint-plugin-no-unary-plus';
-declare module 'eslint-plugin-react-perf';
