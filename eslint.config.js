f8acac5d9b4d605b6a839cfcc8c9eef58ec0277f
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-08-29 12:01:54 +1200

    update repo eslint config

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPlugin from "eslint-plugin-eslint-plugin";
+import tseslint from 'typescript-eslint';
+import nodePlugin from 'eslint-plugin-n';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['src/**/*'],
+    extends: [
+      eslintPlugin.configs.all,
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      nodePlugin.configs["flat/all"],
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    rules: {
+      "import/extensions": "off",
+      "ts/no-unnecessary-condition": "off",
+      "n/no-missing-import": "off",
+      "n/file-extension-in-import": "off",
+    },
+  }
+]);
