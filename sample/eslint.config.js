bb8016620a30da4b27d2f5ba28f6b57172995d19
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-09-13 09:56:26 +1200

    update sample project

--- sample/eslint.config.js
+++ sample/eslint.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'eslint/config';
-
 import { config as bosh, createImportSortRule } from '@bosh/eslint-config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
+    globalIgnores(['./dist']),
+// Base Config
+  {
+    files: ['**/*'],
+    extends: [bosh.configs.base]
+  },
+
+  // TypeScript Config
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [bosh.configs.ts]
+  },
+    // react-app Config
     {
       files: ['examples/react-app/**/*.{js,ts,jsx,tsx}'],
       extends: [bosh.configs.react],
+    
       rules: {
         'simple-import-sort/imports': createImportSortRule({
-          firstGroup: ['react', '^@?\\w'],
+          firstGroup: ['react', String.raw`^@?\w`],
           internalGroups: [
             '^(@/components)(/.*|$)',
             '^(@/hooks)(/.*|$)',
