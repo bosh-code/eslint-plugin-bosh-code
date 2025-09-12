fbb38e19ddf2e7d83dab6173773d102dfe701f86
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-09-13 10:06:34 +1200

    fix linting issues

--- src/rules/invalid-hook-extension/index.ts
+++ src/rules/invalid-hook-extension/index.ts
@@ -1,24 +1,26 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
-import { Linter } from 'eslint';
 import path from 'node:path';
+
+type InvalidHookExtensionRule = ESLintUtils.RuleModule<'invalid-hook-extension', [], unknown, ESLintUtils.RuleListener>
 
 const createRule = ESLintUtils.RuleCreator(
   (name) => `https://github.com/bosh-code/eslint-plugin-bosh/blob/main/src/rules/${name}/README.md`
 );
 
-export const rule = createRule({
+export const rule: InvalidHookExtensionRule = createRule({
   name: 'invalid-hook-extension',
   meta: {
     type: 'problem',
     docs: {
-      description: "enforce hooks files to use '.[j|t]s' extension and not '.[j|t]sx'",
+      description: 'enforce hooks files to use \'.[j|t]s\' extension and not \'.[j|t]sx\'',
+      // @ts-expect-error - ESLint says I need this and TS says I don't.
       recommended: true,
       url: 'https://github.com/bosh-code/eslint-plugin-bosh/blob/main/src/rules/invalid-hook-extension/README.md'
     },
     schema: [],
     messages: {
       'invalid-hook-extension':
-        "Hook files should use .[j|t]s extension, not .[j|t]sx. Hooks typically shouldn't return JSX."
+        'Hook files should use .[j|t]s extension, not .[j|t]sx. Hooks typically shouldn\'t return JSX.'
     }
   },
   defaultOptions: [],
