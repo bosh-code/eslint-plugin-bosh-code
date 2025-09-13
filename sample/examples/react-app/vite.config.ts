c172f8252da94d0253a93c92071c7520a7c1b3e9
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-09-13 23:04:38 +1200

    update paths

--- sample/examples/react-app/vite.config.ts
+++ sample/examples/react-app/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, type AliasOptions } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    } satisfies AliasOptions
+  }
+});
