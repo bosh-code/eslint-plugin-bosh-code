26c80cbc87795d44df681afe60f0fc17c334461b
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-09-13 23:06:01 +1200

    update sample eslint config

--- sample/eslint.config.js
+++ sample/eslint.config.js
@@ -3,29 +3,44 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
     globalIgnores(['./dist']),
-// Base Config
-  {
-    files: ['**/*'],
-    extends: [bosh.configs.base]
-  },
 
-  // TypeScript Config
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [bosh.configs.ts]
-  },
-    // react-app Config
+    // Base Config
+    {
+      files: ['**/*'],
+      extends: [bosh.configs.base]
+    },
+
+    // TypeScript Config
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [bosh.configs.ts]
+    },
+
+    // React config
     {
       files: ['examples/react-app/**/*.{js,ts,jsx,tsx}'],
       extends: [bosh.configs.react],
-    
       rules: {
         'simple-import-sort/imports': createImportSortRule({
           firstGroup: ['react', String.raw`^@?\w`],
           internalGroups: [
             '^(@/components)(/.*|$)',
-            '^(@/hooks)(/.*|$)',
-            '^(@/lib)(/.*|$)'
+            '^(@/hooks)(/.*|$)'
+          ]
+        })
+      }
+    },
+
+    // node config
+    {
+      files: ['examples/express-app/**/*.{js,ts,jsx,tsx}'],
+      extends: [bosh.configs.node],
+
+      rules: {
+        'simple-import-sort/imports': createImportSortRule({
+          firstGroup: ['express', String.raw`^@?\w`],
+          internalGroups: [
+            '^(@src)(/.*|$)'
           ]
         })
       }
