f4967cb359a818d55000809d55758874e8dcfaf6
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-09-15 19:57:40 +1200

    update tsconfig files

--- sample/examples/express-app/config.ts
+++ sample/examples/express-app/config.ts
@@ -1,9 +1,11 @@
 /* eslint-disable n/no-process-env */
-
-import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __filename = fileURLToPath(import.meta.url);
 
 // Check the env
 const NODE_ENV = (process.env.NODE_ENV ?? 'development');
